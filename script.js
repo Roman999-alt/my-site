1b0aded18beb56a1899d963737ea934378a59fd9
Роман999-альт <vojcehovicroma@gmail.com>
2026-03-09 21:56:19 +0300

    added contact form

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function order(){
 alert("Спасибо! Мы скоро свяжемся с вами.");
 }
+document.getElementById("orderForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Спасибо! Ваша заявка отправлена. Мы скоро свяжемся с вами.");
+
+});
